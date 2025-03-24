a9c6668348eacbad305ef6b1d06e9ea617903569
Esma Nur <71775626+esmaakarakuus@users.noreply.github.com>
2025-03-24 13:48:41 +0300

    Update CareersQaPage.java

--- src/main/java/pages/CareersQaPage.java
+++ src/main/java/pages/CareersQaPage.java
@@ -4,27 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class QaCareerPage extends BasePage {
+public class CareersQaPage extends BasePage {
 
-    // WebElement, sayfa üzerindeki öğeleri temsil eder
-    @FindBy(css = "#page-head [class*=btn]") // "See All QA Jobs" butonunu temsil eden WebElement
+    // WebElement
+    @FindBy(css = "#page-head [class*=btn]")
             WebElement seeAllQAJobsBtn;
 
-    // Constructor: Bu constructor, basePage sınıfını çağırarak WebDriver'ı alır ve sayfa nesnelerini başlatır
-    public QaCareerPage(WebDriver driver) {
-        super(driver); // basePage constructor'ını çağırarak WebDriver'ı başlatır
+    // Constructor
+    public CareersQaPage(WebDriver driver) {
+        super(driver);
     }
 
-    // navigateToQA: QA kariyer sayfasına yönlendiren metod
+    // navigateToQA: QA kariyer sayfasına yönlendirir
     public void navigateToQA() {
-        this.driver.get("https://useinsider.com/careers/quality-assurance/"); // QA kariyer sayfasını açar
+        this.driver.get("https://useinsider.com/careers/quality-assurance/");
     }
 
-    // clickSeeAllQAJobsBtn: "See All QA Jobs" butonuna tıklama metodudur
+    // clickSeeAllQAJobsBtn: "See All QA Jobs" butonuna tıklar
     public void clickSeeAllQAJobsBtn() {
-        this.click(seeAllQAJobsBtn); // Butona tıklar
+        this.click(seeAllQAJobsBtn);
     }
-
-
 }
+
 
