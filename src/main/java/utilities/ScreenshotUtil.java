beb1287f67fe9dca49786fc2f2ba31149bff87ae
Esma Nur <71775626+esmaakarakuus@users.noreply.github.com>
2025-03-24 13:59:28 +0300

    Update ScreenshotUtil.java

--- src/main/java/utilities/ScreenshotUtil.java
+++ src/main/java/utilities/ScreenshotUtil.java
@@ -1,4 +1,27 @@
 package utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ScreenshotUtil {
+
+    public static void takeScreenshot(WebDriver driver, String screenshotName) {
+        try {
+            // TakesScreenshot arayüzünü kullanarak ekran görüntüsü al
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+
+            // Ekran görüntüsünü kaydetmek için dosya yolu belirle
+            String screenshotPath = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+            FileUtils.copyFile(source, new File(screenshotPath));
+            System.out.println("Screenshot taken: " + screenshotPath);
+        } catch (IOException e) {
+            System.out.println("Failed to take screenshot: " + e.getMessage());
+        }
+    }
 }
